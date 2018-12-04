b4fa70b51f8ef5195c429d7b2bb233ba12301ef5
cwallace <cwallace@amplify.com>
2018-12-03 21:42:15 -0500

    Bubbles!

---
+++ Assets/Scripts/BubbleGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleGenerator : MonoBehaviour
+{
+
+  public GameObject bubblePrefab;
+  public int maxBubbles;
+  public float timeBetweenSpawnsBase;
+  public float timeBetweenSpawnsVariance; // amount around base that time between spawns can vary
+  float lastSpawnTime;
+  float timeUntilNextSpawn;
+  ShuffleBag<int> spawnRegionsBag; // to control amount of spawns in each region
+
+  public Vector2[] spawnRegions; // x is left edge, y is right edge
+
+  public List<GameObject> spawnedBubbles;
+
+  // Use this for initialization
+  void Start()
+  {
+    spawnRegionsBag = new ShuffleBag<int>();
+    spawnRegionsBag.Add(0, 2);
+    spawnRegionsBag.Add(1, 3);
+    spawnRegionsBag.Add(2, 2);
+    spawnRegionsBag.Add(3, 3);
+    spawnRegionsBag.Add(4, 2);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (lastSpawnTime + timeUntilNextSpawn <= Time.time)
+    {
+      Spawn();
+      lastSpawnTime = Time.time;
+    }
+  }
+
+  void Spawn()
+  {
+    int myRegion = spawnRegionsBag.Next();
+    float spawnXpos = Random.Range(spawnRegions[myRegion].x, spawnRegions[myRegion].y);
+    GameObject newBub = Instantiate(bubblePrefab, new Vector3(spawnXpos, transform.position.y, transform.position.z), Quaternion.identity);
+    spawnedBubbles.Add(newBub);
+    Debug.Log("Spawn at " + spawnXpos + "timeUntilNextSpawn");
+    SetNewSpawnTime();
+  }
+
+  void SetNewSpawnTime()
+  {
+    timeUntilNextSpawn = timeBetweenSpawnsBase + Random.Range(
+      -1 * timeBetweenSpawnsVariance / 2,
+      timeBetweenSpawnsVariance / 2
+    );
+    Debug.Log(timeUntilNextSpawn);
+  }
+
+}
---
+++ Assets/Scripts/ShuffleBag.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// Implements the ShuffleBag data collection
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[System.Serializable]
+public class ShuffleBag<T>
+{
+	private System.Random random = new System.Random();
+	private List<T> data;
+
+	private T currentItem;
+	private int currentPosition = -1;
+
+	public int Capacity
+	{
+		get { return data.Capacity; }
+	}
+
+	public int Size
+	{
+		get { return data.Count; }
+	}
+
+	public ShuffleBag()
+	{
+		data = new List<T>();
+	}
+
+	public ShuffleBag(int initCapacity)
+	{
+		data = new List<T>(initCapacity);
+	}
+
+	/// <summary>
+	/// Add a number of items to the bad.
+	/// </summary>
+	/// <param name="item"> item to add. </param>
+	/// <param name="amount"> amount of items to add. </param>
+	public void Add(T item, int amount)
+	{
+		for (int i = 0; i < amount; i++)
+		{
+			data.Add(item);
+		}
+
+		currentPosition = Size - 1;
+	}
+
+	/// <summary>
+	/// Get an item from the bag.
+	/// </summary>
+	/// <returns> A randomly selected item from the bag. </returns>
+	public T Next()
+	{
+		// If currentPosition is less than one, we reset it to point to the end of the list
+		// and return the first item from the bag. (This covers the situation where we've 
+		// traversed through all the items and now wish to start again.)
+		if (currentPosition < 1)
+		{
+			currentPosition = Size - 1;
+			currentItem = data[0];
+
+			return currentItem;
+		}
+
+		int pos = random.Next(currentPosition);
+		currentItem = data[pos];
+		data[pos] = data[currentPosition];
+		data[currentPosition] = currentItem;
+		currentPosition--;
+
+		return currentItem;
+	}
+}
