d6173c5bcaad3c8122cc4a4f2d0cdfe78e3c8b82
cwallace <cwallace@amplify.com>
2018-12-09 19:09:52 -0500

    Added a rotating bubble just to see what it looks like

---
+++ Assets/Scripts/RotatingBubble.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBubble : MonoBehaviour
+{
+
+  public Sprite rot1;
+  public Sprite rot2;
+  public SpriteRenderer renderer1;
+  public SpriteRenderer renderer2;
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  public float timeToRotate;
+  float lastRotationTime;
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Time.time - lastRotationTime > timeToRotate)
+    {
+      rotate();
+    }
+  }
+
+  void rotate()
+  {
+    Sprite tempSprite = renderer1.sprite;
+    renderer1.sprite = renderer2.sprite;
+    renderer2.sprite = tempSprite;
+    lastRotationTime = Time.time;
+  }
+}
