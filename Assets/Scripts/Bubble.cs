18599e24909048d5d1efc30840a041d9018d90d3
Jack Schlesinger <jackschlesinger@gmail.com>
2018-12-16 11:32:52 -0500

    Mixing complete.

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -87,7 +87,7 @@ public class Bubble : MonoBehaviour
   {
     if (isClaimed)
     {
-      Services.Audio.PlaySoundEffect(Services.Clips.BubblePops[Random.Range(0, Services.Clips.BubblePops.Length)], 0.5f);
+      // Services.Audio.PlaySoundEffect(Services.Clips.BubblePops[Random.Range(0, Services.Clips.BubblePops.Length)], 0.5f);
       if (newColor != ownerColor)
       {
         Destroy(gameObject); // TODO replace with cool animation and sfx
@@ -95,7 +95,7 @@ public class Bubble : MonoBehaviour
     }
     else
     {
-      Services.Audio.PlaySoundEffect(Services.Clips.BubbleClaimed[Random.Range(0, Services.Clips.BubbleClaimed.Length)], 0.5f);
+      Services.Audio.PlaySoundEffect(Services.Clips.BubbleClaimed[Random.Range(0, Services.Clips.BubbleClaimed.Length)], 0.3f);
       isClaimed = true;
       ownerColor = newColor;
       gameObject.GetComponent<SpriteRenderer>().color = ownerColor;//
@@ -136,8 +136,8 @@ public class Bubble : MonoBehaviour
   private void getCaptured()
   {
     EventManager.instance.Raise(new CapturedBubbleEvent());
-    Services.Audio.PlaySoundEffect(Services.Clips.BubbleCaptured[Random.Range(0, Services.Clips.BubbleCaptured.Length)], 0.6f);
-    Services.Audio.PlaySoundEffect(Services.Clips.BubblePops[Random.Range(0, Services.Clips.BubblePops.Length)], 0.75f, Random.Range(0.8f, 1.2f));
+    Services.Audio.PlaySoundEffect(Services.Clips.BubbleCaptured[Random.Range(0, Services.Clips.BubbleCaptured.Length)], 0.35f);
+    Services.Audio.PlaySoundEffect(Services.Clips.BubblePops[Random.Range(0, Services.Clips.BubblePops.Length)], 0.5f, Random.Range(0.8f, 1.2f));
     // do cool visual effects
     ownerColor = Color.white;
     gameObject.GetComponent<SpriteRenderer>().color = ownerColor;
